b87f0e9e5abd04a62bad8ae2485c3d0bb7fbcbcd
MUKILTEO\1717998 <1717998@mukilteo.wednet.edu>
2024-11-05 09:38:23 -0800

    yer

---
+++ Advanced Text Adventure/Abstraction.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_Text_Adventure
+{
+    
+    }
+
+    //Players is the broad class for all the specific classes like Benicio and Piper
+    // 'abstract' makes it so you can't use it but it can still be used to add things that go to all individuals of the class so you don't have to add it to every class
+    public abstract class Players
+    {
+        public int age;
+        public (float r, float g, float b) hairColor;
+        public string name;
+         
+        
+        //"protected" means anything in the class or connected to the class has access to it
+        //unlike private where it's only availible in that class or public where it is availble anywhere
+        //protected int example;
+    }
+    
+    //added Players to Benicio so it is conected to the Players class
+    //So anything with ": Players" anything in the Players class goes to all with that Players thing
+    public class Benicio : Players 
+    {
+
+    public float brainPower;
+
+    }
+    
+    public class  Thomas : Players
+    {
+
+    public float SSJscale;
+
+    }
+    
+    public class Cayden : Players
+    {
+
+    public float mafiaBossScale;
+
+    }
+
+    public class Braden : Players
+    {
+
+    public float basicScale;
+
+    }
+     public class Jaden : Players
+    {
+
+    public float fatScale;
+
+    }
+    public class Ruth : Players
+    {
+
+    public float stupidLevel;
+
+    }
+
+    public class Maliyah : Players
+    {
+    public float evilTyrant;
+    }
+     public class Alex : Players
+    {
+    public float fakeQueer;
+    }
+     public class Piper : Players
+    {
+    public float obsessive;
+    }
+    
+    
+    internal class Abstraction
+    {
+        static void Main(string[] args)
+        {
+            Benicio benicio = new() {age = 17, name = ("Benicio also known as Joe Biden"), hairColor = (0, 0, 0) };
+            Thomas thomas = new() { age = 18, name = ("Thomas also known as the creature >:) "), hairColor = (0, 0, 0) };
+            Cayden cayden = new() { age = 17, name = ("Cayden 🤓"), hairColor = (0, 0, 0) };
+            Jaden jaden = new() { age = 17, name = ("Jaden also known as nerdbob"), hairColor = (196, 164, 132) };
+            Braden braden = new() { age = 17, name = ("ඞ"), hairColor = (196, 164, 132) };
+            Ruth ruth = new() { age = 17, name = ("Ruth also known as BrothBones☠︎"), hairColor = (92, 64, 51) };
+            Piper piper = new() { age = 17 , name = ("Piper also known as Grim"), hairColor = (0, 0, 0) };
+            Maliyah maliyah = new() { age = 17 , name = ("Maliyah also known as Lord Megatron"), hairColor = (92, 64, 51) };
+            Alex alex = new() { age = 17, name = ("Alex eight handled sword divergent sila divine general mahoraga"), hairColor = (223, 195, 147)};
+
+        List<Players> players = [benicio, thomas, cayden, jaden, braden, ruth, piper, maliyah, alex];
+            
+            //foreach (Players animal in animals)
+            //{
+            //    animal.Speak();
+            //}
+            
+            
+
+            
+        }
+    }
+
---
+++ Advanced Text Adventure/Collection.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_Text_Adventure
+{
+    internal class Collection
+    {
+        static void Collect()
+        {
+            // some basic variable types
+            int x = 5;
+            float y = 3.5f;
+            string myString = "hi";
+            char c = 'I';
+            bool myBool = true;
+
+            //Array. The size never changes.
+            bool[] myBools = [true, false, false, true];
+            myBools[0] = false;
+            myBools[3] = false;
+            Console.WriteLine(myBools[0]); //false
+
+            //List. the size grows and shrinks as necceassary.
+            List<bool> boolList = [false, true];
+            boolList.Add(false);
+            boolList.Add(true);
+            boolList.Remove(true);
+            Console.WriteLine(boolList[2]); //true
+
+            // Stack. Grows and Shrinks. Last-in-fist-out
+            Stack<string> myStack = new();
+            myStack.Push("Help");
+            myStack.Push(", ");
+            myStack.Push("Please");
+
+            Console.WriteLine(myStack.Pop()); // Please
+            Console.WriteLine(myStack.Pop()); //, 
+            Console.WriteLine(myStack.Peek()); // Help
+            Console.WriteLine(myStack.Peek()); // Help
+            Console.WriteLine(myStack.Pop());  // Help
+
+            // Queue. Grows and Shrinks. first-in-first-out.
+
+            Queue<string> myQueue = new();
+            myQueue.Enqueue("Help");
+            myQueue.Enqueue(", ");
+            myQueue.Enqueue("Please");
+
+            Console.WriteLine(myQueue.Dequeue()); //Help
+            Console.WriteLine(myQueue.Dequeue()); //, 
+            Console.WriteLine(myQueue.Peek()); //Please
+            Console.WriteLine(myQueue.Peek()); //Please
+            Console.WriteLine(myQueue.Dequeue()); //Please
+
+            //Dictionary. stores a key-value collection. looks up a value given a key.
+            Dictionary<string, int> myDictinary = new()
+            {
+                { "Joe", 71 },
+                { "Joseph", 50 }
+            };
+
+            Console.WriteLine(myDictinary["Joe"]); //71
+            Console.WriteLine(myDictinary["Joeseph"]); //50
+        }
+    }
+}
